0c59fa5cfddb84297b6cc7d8b5ca48dc9f6d3764
lytecode <mbonubasil@gmail.com>
2021-08-05 00:00:48 +0100

    Add mapped types

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
