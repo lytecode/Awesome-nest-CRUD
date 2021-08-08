6e4375f59135f019339029e41f2bed34c3300d38
lytecode <mbonubasil@gmail.com>
2021-08-08 09:05:19 +0100

    Setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Setup swagger docs
+  const config = new DocumentBuilder()
+    .setTitle('Awesome Nest API')
+    .setDescription('CRUD API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
