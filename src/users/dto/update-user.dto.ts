8cb819ab938520d641b817922d5800843be097af
Onoja <onojamatthew59@gmail.com>
2021-08-14 13:53:09 +0100

    Use class validator

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
+  @IsString()
+  @MinLength(2)
   name: string;
 }
