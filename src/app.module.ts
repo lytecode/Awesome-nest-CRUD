1e9b6c5b47c4c1440f21b4a7d0cfcaad15946c0f
Onoja <onojamatthew59@gmail.com>
2021-08-17 08:08:04 +0100

    Setup user entity with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import config from './database/ormConfig';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [ProductsModule, UsersModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,13 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class User {
   @ApiProperty()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @ApiProperty()
+  @Column()
   name: string;
 }
