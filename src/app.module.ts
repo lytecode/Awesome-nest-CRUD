b82fc698bc57b1e6a7a7d3dc426bfe32a299a95a
Mbonu Basil <mbonubasil@gmail.com>
2021-09-04 19:13:13 +0100

    Remove db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import config from './database/ormConfig';
 
 @Module({
-  imports: [UsersModule], //TypeOrmModule.forRoot()],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,14 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
 export class User {
   @ApiProperty()
-  @PrimaryGeneratedColumn()
   id: number;
 
-  @ApiProperty()
-  @Column()
   firstName: string;
 
   lastName: string;
