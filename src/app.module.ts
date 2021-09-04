f0e008085aedcee34eab700085e81b5d2a34f7dd
Mbonu Basil <mbonubasil@gmail.com>
2021-09-04 17:41:37 +0100

    Remove db connection for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import config from './database/ormConfig';
 
 @Module({
-  imports: [ProductsModule, UsersModule, TypeOrmModule.forRoot(config)],
+  imports: [UsersModule], //TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
