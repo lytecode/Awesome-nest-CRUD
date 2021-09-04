da288708db6004c010f1885fc2da6eb1e6d3d4e7
Mbonu Basil <mbonubasil@gmail.com>
2021-09-04 18:05:57 +0100

    Use mapped-type to reduce duplication in updateUserdto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,14 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MinLength } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @ApiProperty()
-  @IsString()
-  @MinLength(2)
-  readonly firstName?: string;
-  readonly lastName?: string;
-  readonly email?: string;
-  readonly password?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
